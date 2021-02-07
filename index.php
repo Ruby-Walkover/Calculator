619d64b0fa1ed2f8e070a049a61adc620cde8db1
Ruby-Walkover <ruby@whozzat.com>
2021-02-07 09:19:00 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <form action="functions.php" method="get">
+    <input type="text" name="num01" placeholder="Number 1">
+    <select name="oper">
+    <label>Choose Operation</label>
+    <option value="add">Add</option>
+    <option value="sub">Subtract</option>
+    </select>
+    <input type="text" name="num02" placeholder="Number 2"> 
+    <button type="submit">Calculate</button>
+    </form>
+</body>
+</html>
